362b13859f550ca76d57ffbeb114f43df013db78
Виталий Прончатов <vitalyaudit01@gmail.com>
2020-08-24 10:36:35 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal d = 4;
+
+            Console.WriteLine(d);
+            Console.WriteLine("Переменная d имеет тип: " + d.GetType());
+            Console.WriteLine("Максимальное значение типа decimal равно: ");
+            Console.WriteLine(decimal.MaxValue);
+            Console.ReadKey();
+        }
+    }
+}
